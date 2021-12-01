6f74e1531b0816e7e7cddb1f4f38a4ad1510393d
chaerin <newfkxpdlf@naver.com>
2021-12-01 17:46:23 +0900

    Practice06 ~ex07

--- src/com/javaex/ex07/SmartPhone.java
+++ src/com/javaex/ex07/SmartPhone.java
@@ -5,6 +5,13 @@ public class SmartPhone extends MusicPhone {
     public void execute(String str){
         
         //코드작성
+    	if("앱".equals(str)) {
+    		System.out.println("앱실행");
+    	}else if("음악".equals(str)) {
+    		System.out.println("다운로드해서 음악재생");
+    	}else {
+    		super.execute(str);
+    	}
         
     }
  
